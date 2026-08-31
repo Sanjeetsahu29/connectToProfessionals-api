aea538dbdff02d302ec9458142049a5d3cbc116f
dev.sanjeetsahu <sanjeetsahus29@gmail.com>
2026-08-31 18:19:58 +0530

    add a test api to test from the postman app

--- src/app.js
+++ src/app.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/test', (req, res) => {
+  res.send('Hello from the test page!');
+});
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
