fbe23e2e344e743114c27b053c1338d553b8f7fc
dev.sanjeetsahu <sanjeetsahus29@gmail.com>
2026-09-01 18:05:51 +0530

    add express.json() middleware in the main backend file to parse the incoming request from raw json stream to javaScript object

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const connectDB = require("./config/database");
 const User = require("./models/user.model");
 const dotenv = require("dotenv");
 dotenv.config();
+app.use(express.json());
 
 const port = process.env.PORT || 4000;
 app.post("/signup", async (req, res) => {
@@ -16,18 +17,18 @@ app.post("/signup", async (req, res) => {
   // });
   console.log("Request body:", req.body);
 
-  try {
-    const savedUser = await newUser.save();
-    res.status(201).json({
-      message: "New user saved successfully to the database",
-      user: savedUser,
-    });
-  } catch (err) {
-    res.status(500).json({
-      message: "Error saving user to the database",
-      error: err.message,
-    });
-  }
+  // try {
+  //   const savedUser = await newUser.save();
+  //   res.status(201).json({
+  //     message: "New user saved successfully to the database",
+  //     user: savedUser,
+  //   });
+  // } catch (err) {
+  //   res.status(500).json({
+  //     message: "Error saving user to the database",
+  //     error: err.message,
+  //   });
+  // }
 });
 
 connectDB()
