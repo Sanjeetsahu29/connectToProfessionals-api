b9a67aecfbcbb588f8bf5ffb66c578a268b0e381
dev.sanjeetsahu <sanjeetsahus29@gmail.com>
2026-09-01 19:43:10 +0530

    add api for finding user by email, all users and deleting the user by email

--- src/app.js
+++ src/app.js
@@ -7,28 +7,95 @@ dotenv.config();
 app.use(express.json());
 
 const port = process.env.PORT || 4000;
-app.post("/signup", async (req, res) => {
-  //creating a new instance of the User model with the data
-  // const newUser = new User({
-  //   firstName: "Sanjeet",
-  //   lastName: "Sahu",
-  //   email: "Sanjeet@gmail.com",
-  //   password: "Sanjeet@123",
-  // });
-  console.log("Request body:", req.body);
 
-  // try {
-  //   const savedUser = await newUser.save();
-  //   res.status(201).json({
-  //     message: "New user saved successfully to the database",
-  //     user: savedUser,
-  //   });
-  // } catch (err) {
-  //   res.status(500).json({
-  //     message: "Error saving user to the database",
-  //     error: err.message,
-  //   });
-  // }
+app.get("/user", async (req, res) => {
+  const { emailID } = req.body;
+  try {
+    const user = await User.findOne({ email: emailID });
+    console.log(user);
+    if (user) {
+      res.status(200).json({
+        message: "User found",
+        user: user,
+      });
+    } else {
+      res.status(404).json({
+        message: "User not found",
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "Error finding user",
+      error: err.message,
+    });
+  }
+});
+
+app.get("/users", async (req, res) => {
+  try {
+    const allUsers = await User.find({});
+    if (allUsers.length === 0) {
+      res.status(200).json({
+        message: "No users found",
+        users: allUsers,
+      });
+    } else {
+      res.status(200).json({
+        message: `Total ${allUsers.length} users are there in the database`,
+        users: allUsers,
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "Error finding users",
+      error: err.message,
+    });
+  }
+});
+
+app.post("/signup", async (req, res) => {
+  const { firstName, lastName, email, password } = req.body;
+  const newUser = new User({
+    firstName: firstName,
+    lastName: lastName,
+    email: email,
+    password: password,
+  });
+
+  try {
+    const savedUser = await newUser.save();
+    res.status(201).json({
+      message: "New user saved successfully to the database",
+      user: savedUser,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: "Error saving user to the database",
+      error: err.message,
+    });
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  const { emailID } = req.body;
+  try {
+    const deletedUser = await User.findOneAndDelete({ email: emailID });
+    if (deletedUser) {
+      res.status(200).json({
+        message: "User deleted successfully",
+        user: deletedUser,
+      });
+    } else {
+      res.status(404).json({
+        message: "User not found",
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "Error deleting user",
+      error: err.message,
+    });
+  }
 });
 
 connectDB()
