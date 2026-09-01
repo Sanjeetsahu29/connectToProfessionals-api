da0f2a8f5eb250bd31a66bf1e64cf46ac588e4d7
dev.sanjeetsahu <sanjeetsahus29@gmail.com>
2026-09-01 19:56:40 +0530

    add a patch api to modify the existing field's value of a user

--- src/app.js
+++ src/app.js
@@ -98,6 +98,32 @@ app.delete("/user", async (req, res) => {
   }
 });
 
+app.patch("/user", async (req, res) => {
+  const { emailID, updateData } = req.body;
+  try {
+    const updateUser = await User.findOneAndUpdate(
+      { email: emailID },
+      updateData,
+      { new: true },
+    );
+    if (updateUser) {
+      res.status(200).json({
+        message: "User updated successfully",
+        user: updateUser,
+      });
+    } else {
+      res.status(404).json({
+        message: "User not found",
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "Error updating user",
+      error: err.message,
+    });
+  }
+});
+
 connectDB()
   .then(() => {
     console.log("Connected to the database");
