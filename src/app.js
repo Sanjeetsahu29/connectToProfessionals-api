b52dac12bd4aba7e19f0ddbce0b358fcf6537a50
dev.sanjeetsahu <sanjeetsahus29@gmail.com>
2026-09-01 16:16:12 +0530

    add a basic signup api with post http method to add a new unique user and throw a error message on duplicate entry of user

--- src/app.js
+++ src/app.js
@@ -2,6 +2,30 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const connectDB = require("./config/database");
+const User = require("./models/user.model");
+
+app.post("/signup", async (req, res) => {
+  //creating a new instance of the User model with the data
+  const newUser = new User({
+    firstName: "Sanjeet",
+    lastName: "Sahu",
+    email: "Sanjeet@gmail.com",
+    password: "Sanjeet@123",
+  });
+
+  try {
+    const savedUser = await newUser.save();
+    res.status(201).json({
+      message: "New user saved successfully to the database",
+      user: savedUser,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: "Error saving user to the database",
+      error: err.message,
+    });
+  }
+});
 
 connectDB()
   .then(() => {
--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   await mongoose.connect(
-    "mongodb+srv://sanjeet_db:mkLbH7SPjzMxX5i6@connecttoprofessional.tfs2df4.mongodb.net/",
+    "mongodb+srv://sanjeet_db:mkLbH7SPjzMxX5i6@connecttoprofessional.tfs2df4.mongodb.net/connectToProfessional",
   );
 };
 
