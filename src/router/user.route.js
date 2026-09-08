c130856b694db5a75cf977035a907edd3f331df4
dev.sanjeetsahu <sanjeetsahus29@gmail.com>
2026-09-08 11:28:08 +0530

    add a pagination logic and proper query input validation to protect this feed api from breaking

--- src/router/user.route.js
+++ src/router/user.route.js
@@ -161,6 +161,11 @@ userRouter.get("/feed", userAuth, async (req, res) => {
   try {
     const loggedInUser = req.user;
 
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    limit = limit > 20 ? 20 : limit;
+    const skip = (page - 1) * limit;
+
     // Find all connection requests involving the logged-in user
     const connectionRequests = await ConnectionRequest.find({
       $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
@@ -182,9 +187,12 @@ userRouter.get("/feed", userAuth, async (req, res) => {
       _id: {
         $nin: Array.from(hideUserFromFeed),
       },
-    }).select(
-      "firstName lastName gender age about profilePhoto skills interests",
-    );
+    })
+      .select(
+        "firstName lastName gender age about profilePhoto skills interests",
+      )
+      .skip(skip)
+      .limit(limit);
 
     return res.status(200).json({
       message: "Feed users fetched successfully",
